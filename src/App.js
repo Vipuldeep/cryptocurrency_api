9a95502f7ed04f2723e5e2fe6a708fc1f459cd01
Vipul <vipuldeep95@gmail.com>
2021-04-12 10:21:18 +1000

    fetching api to the screen using axios and sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useState , useEffect} from 'react';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
+import Coin from './Coin';
 
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false
 // api promises get and then for coin currency
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -16,16 +18,35 @@ function App() {
     })
     // error catch in console
     .catch(error => console.log(error));
-  })
+  }, [])  ;
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="coin-app">
       <div className="coin-search">
     <h1>Search for coin currency</h1>
     <form>
-      <input type="text" placeholder="search currency" className="coin-input"></input>
+      <input type="text" placeholder="search currency" className="coin-input" onChange={handleChange}></input>
     </form>
       </div>
+      {filteredCoins.map(coin => {
+        return <Coin key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                price={coin.current_price}
+                volume={coin.total_volume}
+                priceChange={coin.price_change_percentage_24h}
+                marketcap={coin.market_cap}
+          />;
+      })}
     </div>
   );
 }
